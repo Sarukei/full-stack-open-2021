73af7442cc321057516f40128692dfd62d996609
Sarukei <36385459+Sarukei@users.noreply.github.com>
2021-07-20 16:29:31 -0500

    Part1 (#1)
    * Finish 1.1: course information, step1
    
    * Finish 1.2: course information, step2
    
    * Finish 1.3: course information step3
    
    * Finish 1.4: course information step4
    
    * Finish 1.5: course information step5
    
    * Finish 1.6: unicafe step1
    
    * Finish unicafe step2
    
    * Finish 1.9: unicafe step 3 - 5
    
    * Finish 1.11*: unicafe step6
    
    * Finish 1.12*: anecdotes step1
    
    * Finish 1.13*: anecdotes step2
    
    * 1.14*: anecdotes step3

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const Section = (props) => <h2>{props.title}</h2>;
+
+const Button = (props) => {
+  return <button onClick={props.clickHandler}>{props.text}</button>;
+};
+
+const Statistic = (props) => {
+  return (
+    <tr>
+      <td>{props.title}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({
+  stat1,
+  stat2,
+  stat3,
+  totalFeedback,
+  avgFeedback,
+  positiveFeedbackPercentage,
+}) => {
+  const feedbackGiven = stat1.value + stat2.value + stat3.value > 0;
+
+  if (feedbackGiven) {
+    return (
+      <table>
+        <tbody>
+          <Statistic title={stat1.title} value={stat1.value} />
+          <Statistic title={stat2.title} value={stat2.value} />
+          <Statistic title={stat3.title} value={stat3.value} />
+          <Statistic title="all" value={totalFeedback} />
+          <Statistic title="average" value={avgFeedback} />
+          <Statistic title="positive" value={positiveFeedbackPercentage} />
+        </tbody>
+      </table>
+    );
+  }
+  return <p>"No feedback given"</p>;
+};
+
+const App = () => {
+  // Save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const totalFeedback = good + neutral + bad;
+  const avgFeedback = (good - bad) / totalFeedback;
+  const positiveFeedbackPercentage = (good / totalFeedback) * 100 + "%";
+
+  const handleGoodFeedback = () => setGood(good + 1);
+  const handleNeutralFeedback = () => setNeutral(neutral + 1);
+  const handleBadFeedback = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Section title="give feedback" />
+      <Button clickHandler={handleGoodFeedback} text="good" />
+      <Button clickHandler={handleNeutralFeedback} text="neutral" />
+      <Button clickHandler={handleBadFeedback} text="bad" />
+
+      <Section title="statistics" />
+
+      <Statistics
+        stat1={{ title: "good", value: good }}
+        stat2={{ title: "neutral", value: neutral }}
+        stat3={{ title: "bad", value: bad }}
+        totalFeedback={totalFeedback}
+        avgFeedback={avgFeedback}
+        positiveFeedbackPercentage={positiveFeedbackPercentage}
+      />
+    </div>
+  );
+};
+
+export default App;
