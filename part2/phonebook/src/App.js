e2bf683cc4a08ce0e72ff22111767190c0d52a33
Sarukei <xSarukei@gmail.com>
2021-07-24 12:56:52 -0500

    Finish 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    // { name: "Ada Lovelace", number: "39-44-5323523" },
+    // { name: "Dan Abramov", number: "12-43-234345" },
+    // { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const personAlreadyExists = (name) =>
     persons.find((person) => person.name === name);
 
-  const handleSubmit = (e) => {
+  const handleNewPerson = (e) => {
     e.preventDefault();
 
     // Check if person already exists
@@ -19,25 +25,37 @@ const App = () => {
 
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     setPersons([...persons, newPerson]);
 
     // Clear the new name value from the input
     setNewName("");
+    // Clear the number value from the input
+    setNewNumber("");
   };
 
-  const handleChange = (e) => {
+  const handleNewName = (e) => {
     const nameValue = e.target.value;
 
     setNewName(nameValue);
   };
 
+  const handleNewNumber = (e) => {
+    const numberValue = e.target.value;
+    setNewNumber(numberValue);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleNewPerson}>
         <div>
-          name: <input onChange={handleChange} value={newName} />
+          name: <input onChange={handleNewName} value={newName} />
+        </div>
+
+        <div>
+          number: <input onChange={handleNewNumber} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -45,7 +63,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.length
-        ? persons.map((person, i) => <p key={person.name}>{person.name}</p>)
+        ? persons.map((person, i) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))
         : "..."}
     </div>
   );
