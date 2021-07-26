e6c6d852198c61c7202ab008742a7ba88d6c18ae
Sarukei <xSarukei@gmail.com>
2021-07-26 09:47:19 -0500

    Finish 3.7: Phonebook backend step7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 let persons = [
@@ -26,6 +27,7 @@ let persons = [
 const PORT = 3001;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
