30c5645bc4f16fb536cf8a72b7f61aee0e780db7
Sarukei <xSarukei@gmail.com>
2021-07-26 08:53:34 -0500

    Finish 3.4: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -38,6 +38,19 @@ app.get(`/api/persons/:id`, (req, res) => {
   }
 
   res.json(person);
+});
+
+app.delete(`/api/persons/:id`, (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    return res.status(204).end();
+  }
+
+  persons = persons.filter((person) => person.id !== id);
+
+  return res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
