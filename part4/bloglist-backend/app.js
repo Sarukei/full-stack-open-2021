740d4c43de24567336a2604f9256579f851e902b
Sarukei <xSarukei@gmail.com>
2021-08-03 10:31:04 -0500

    Finish 4.20*: bloglist expansion, step8

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -27,6 +27,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
