2e21b8904fdc699bc291d13200ecb5e8d2de3ba5
Sarukei <xSarukei@gmail.com>
2021-08-03 10:23:42 -0500

    Finish 4.18: bloglist expansion, step6 and 4.19: bloglist expansion, step7

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogRouter = require("./controllers/blog");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const app = express();
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
@@ -27,7 +28,8 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
-app.use("/api/users/", usersRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
 
--- part4/bloglist-backend/controllers/blog.js
+++ part4/bloglist-backend/controllers/blog.js
@@ -1,5 +1,5 @@
 const blogRouter = require("express").Router();
-
+const jwt = require("jsonwebtoken");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
@@ -13,9 +13,27 @@ blogRouter.get("/", async (request, response) => {
   return response.json(foundBlogs);
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: "token missing or invalid",
+    });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
